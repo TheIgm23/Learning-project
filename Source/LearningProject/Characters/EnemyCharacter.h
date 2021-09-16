3c9ae177287b694b28ea9760dbd3a0d3f931c71a
Imanol <23imanol23@gmail.com>
2021-09-16 18:20:24 +0200

    Characters created and Engine set up

---
+++ Source/LearningProject/Characters/EnemyCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LearningProject/LearningProjectCharacter.h"
+#include "EnemyCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEARNINGPROJECT_API AEnemyCharacter : public ALearningProjectCharacter
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/LearningProject/Characters/PlayerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LearningProject/LearningProjectCharacter.h"
+#include "PlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEARNINGPROJECT_API APlayerCharacter : public ALearningProjectCharacter
+{
+	GENERATED_BODY()
+	
+};
